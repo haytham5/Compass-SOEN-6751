87436172e20ce3f5815e5300aaf55dc56504d458
haytham5 <78439856+haytham5@users.noreply.github.com>
2026-02-21 22:42:02 -0500

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,246 @@
+import { Pacifico_400Regular, useFonts } from "@expo-google-fonts/pacifico";
+import AppLoading from "expo-app-loading";
+import { LinearGradient } from "expo-linear-gradient";
+import * as NavigationBar from "expo-navigation-bar";
+import { useEffect } from "react";
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import MapView, { Polygon } from "react-native-maps";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+export default function Index() {
+  let [fontsLoaded] = useFonts({
+    Pacifico_400Regular,
+  });
+
+  useEffect(() => {
+    NavigationBar.setBackgroundColorAsync("#F7F9FF");
+    NavigationBar.setButtonStyleAsync("dark");
+  }, []);
+  NavigationBar.setBehaviorAsync("overlay-swipe");
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <SafeAreaView style={styles.background}>
+      <StatusBar backgroundColor="#FFFFFF" barStyle="dark-content" />
+
+      {/* Header */}
+      <View style={styles.header}>
+        <Text style={styles.title}>Your Map</Text>
+
+        <View style={styles.userCircle}>
+          <Icon name="person" size={20} color="white" />
+        </View>
+      </View>
+
+      {/* Map */}
+      <View style={styles.mapWrapper}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 45.4973,
+            longitude: -73.579,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+          showsUserLocation
+          showsMyLocationButton
+        >
+          <Polygon
+            coordinates={[
+              { latitude: 45.49716811115521, longitude: -73.579546473066 },
+              { latitude: 45.497704051422694, longitude: -73.57903222820022 },
+              { latitude: 45.49737047268203, longitude: -73.57833980200657 },
+              { latitude: 45.49683769116792, longitude: -73.57884953594419 },
+            ]}
+            fillColor="rgba(39, 99, 137, 0.35)"
+            strokeColor="#276389"
+            strokeWidth={2}
+          />
+
+          {/* <Marker
+            coordinate={{ latitude: 45.4969, longitude: -73.5786 }}
+            title="Hall Building"
+          />
+          <Marker
+            coordinate={{ latitude: 45.4978, longitude: -73.5796 }}
+            title="Library Building"
+          />
+          <Marker
+            coordinate={{ latitude: 45.4965, longitude: -73.5799 }}
+            title="EV Building"
+          /> */}
+        </MapView>
+
+        <TouchableOpacity style={styles.addReport}>
+          <Icon name="add-circle" size={24} color="#276389" />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.reportFilters}>
+          <Icon name="filter-alt" size={24} color="#276389" />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.relaxMode}>
+          <Icon name="all-inclusive" size={24} color="#276389" />
+        </TouchableOpacity>
+      </View>
+
+      {/* Bottom Navigation */}
+      <View>
+        <LinearGradient
+          colors={["#F7F9FF", "#FFFFFF", "#FFFFFF"]}
+          style={styles.bottomNav}
+        >
+          <TouchableOpacity style={styles.navItem}>
+            <Icon name="home" size={26} color="#276389" />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.navItem}>
+            <Icon name="calendar-month" size={26} color="#276389" />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.navItem}>
+            <Icon name="notifications" size={26} color="#276389" />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.navItem}>
+            <Icon name="settings" size={26} color="#276389" />
+          </TouchableOpacity>
+        </LinearGradient>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    justifyContent: "space-between",
+  },
+
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingTop: 10,
+  },
+
+  marker: {
+    backgroundColor: "#276389",
+    padding: 2,
+    borderRadius: 20,
+    elevation: 5,
+  },
+
+  addReport: {
+    position: "absolute",
+    bottom: 75,
+    left: 15,
+    backgroundColor: "white",
+    padding: 12,
+    borderRadius: 20,
+
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+
+  relaxMode: {
+    position: "absolute",
+    bottom: 15,
+    right: 15,
+    backgroundColor: "white",
+    padding: 12,
+    borderRadius: 20,
+
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+
+  reportFilters: {
+    position: "absolute",
+    bottom: 15,
+    left: 15,
+    backgroundColor: "white",
+    padding: 12,
+    borderRadius: 20,
+
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+
+  title: {
+    fontSize: 28,
+    fontFamily: "Pacifico_400Regular",
+  },
+
+  userCircle: {
+    width: 45,
+    height: 45,
+    borderRadius: 25,
+    backgroundColor: "#276389",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  mapWrapper: {
+    flex: 1,
+    margin: 20,
+    borderRadius: 20,
+    overflow: "hidden",
+    backgroundColor: "#ddd",
+    ...Platform.select({
+      ios: {
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 1,
+      },
+      android: {
+        elevation: 1,
+        shadowColor: "#000",
+      },
+    }),
+  },
+
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+
+  bottomNav: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    paddingVertical: 15,
+    // backgroundColor: "#F7F9FF",
+    borderTopWidth: 1,
+    borderTopColor: "#DDE3EA",
+  },
+
+  navItem: {
+    alignItems: "center",
+  },
+
+  navText: {
+    fontSize: 12,
+    marginTop: 4,
+    color: "#888",
+  },
+});
